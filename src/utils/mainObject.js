79543c23d969fec21a650e7cf5845787904cff70
Felipe Garzon <felipegarxon@hotmail.com>
2021-03-20 23:53:55 -0500

    :100:  Complete view and add functionality

---
+++ src/utils/mainObject.js
@@ -0,0 +1,100 @@
+const mainObject = [
+    {
+        label: '7',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:7',
+    },
+    {
+        label: '8',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:8',
+    },
+    {
+        label: '9',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:9',
+    },
+    {
+        label: 'X',
+        empty: false,
+        color: 'secondary',
+        operation: 'addOperation:multiplicacion',
+    },
+    {
+        label: '4',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:4',
+    },
+    {
+        label: '5',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:5',
+    },
+    {
+        label: '6',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:6',
+    },
+    {
+        label: '-',
+        empty: false,
+        color: 'secondary',
+        operation: 'addOperation:resta',
+    },
+    {
+        label: '1',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:1',
+    },
+    {
+        label: '2',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:2',
+    },
+    {
+        label: '3',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:3',
+    },
+    {
+        label: '+',
+        empty: false,
+        color: 'secondary',
+        operation: 'addOperation:suma',
+    },
+    {
+        label: 'CE',
+        empty: false,
+        color: 'secondary',
+        operation: 'addOperation:limpiar',
+    },
+    {
+        label: '0',
+        empty: false,
+        color: 'primary',
+        operation: 'addNumber:0',
+    },
+    {
+        label: '=',
+        empty:false,
+        color: 'secondary',
+        operation: 'addOperation:igualA',
+    },
+    {
+        label: '/',
+        empty: false,
+        color: 'secondary',
+        operation: 'addOperation:division',
+    },
+]
+
+export default mainObject;
